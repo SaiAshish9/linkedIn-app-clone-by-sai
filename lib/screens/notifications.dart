928659e2484db02e36caf288325b7dfff5cbe7ff
saiashish9 <saiashish3760@gmail.com>
2021-02-25 04:21:49 +0530

    draft post component updated

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:linkedIn/components/nav.dart';
+import 'package:linkedIn/constants/colors.dart';
 
 class Notifications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: null,
+      body: Container(
+        color: kHomePageBackground,
+        child: Column(
+          children: [Nav()],
+        ),
+      ),
     );
   }
 }
